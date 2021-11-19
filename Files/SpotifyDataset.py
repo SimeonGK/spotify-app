924af29f274b9fcdb2481f541156a0ad45b23252
s2656205 <b.ozdemir@umail.leidenuniv.nl>
2021-11-19 12:13:45 +0100

    Toegevoegd Jupyter

--- Files/SpotifyDataset.py
+++ Files/SpotifyDataset.py
@@ -14,36 +14,27 @@ import os, sys
 import matplotlib.pyplot as plt
 sns.set(color_codes=True)
 
-
 df = pd.read_csv('C:/Users/bnkoz/AppData/Local/GitHubDesktop/app-2.9.3/spotify-app/Files/spotify_dataset.csv')
 df = df.iloc[:500]
 
+class Spotifyapp:
+    
+
+    def initialize_column(self):
+        print('Choose one of the options you would like to compare:')
+
+        for columns in df.columns:
+            print(columns)
+        
+        col_input_one = input()
+        print('And:/n')
+        col_input_two = input()
+        
+        return str(col_input_one), str(col_input_two)
 
 
-selection = df.iloc[:500,:2]
-selection.columns = ['X', 'y']
-#print(df['Highest Charting Position'])
+x = Spotifyapp()
+variable_one = x.initialize_column()
 
-print('Choose one of the options you would like to compare:')
-for chooseCol in df.columns:
-        print(chooseCol)
-colInput1 = input()
-print('And:/n')
-colInput2 = input()
+print(" x is ", variable_one)
 
-
-
-print(df[[colInput1, colInput2]].corr)
-
-plt.scatter(df[colInput1], df[colInput2], marker='o')
-plt.xlabel([0, 100])
-plt.show()
-
-
-
-#spfy = FirstPart['Highest Charting Position']
-#spfy2 = FirstPart['Valence']
-#print("Highest Charting Position", spfy, "Valence", spfy2)
-#print(spfy, spfy2)
-#plt.plot(spfy, spfy2, color='red', scalex=True, scaley=True)
-#plt.show()
